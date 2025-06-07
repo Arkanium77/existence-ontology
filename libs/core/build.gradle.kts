35af7ef20de0afdfa3bc9bb939835b9a2ba2a139
Arkanium77 <Arkanium77@gmail.com>
2025-06-07 16:30:07 +0300

    Configure Maven publishing and Gradle properties

--- libs/core/build.gradle.kts
+++ libs/core/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     java
+    `maven-publish`
+    signing
 }
 
 group = "team.isaz.existence"
@@ -18,4 +20,54 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+            pom {
+                name.set("core")
+                description.set("Existence Ontology core utilities")
+                url.set("https://github.com/Arkanium77/ExistenceOntology")
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://opensource.org/licenses/MIT")
+                    }
+                }
+                developers {
+                    developer {
+                        // developer ID обычно совпадает с ником
+                        id.set("Arkanium77")
+                        name.set("Arkanium77")
+                        email.set("arkanium77@gmail.com")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com/Arkanium77/ExistenceOntology.git")
+                    developerConnection.set("scm:git:ssh://github.com:Arkanium77/ExistenceOntology.git")
+                    url.set("https://github.com/Arkanium77/ExistenceOntology")
+                }
+            }
+        }
+    }
+    repositories {
+        maven {
+            name = "Sonatype"
+            url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+            credentials {
+                username = project.findProperty("ossrhUsername") as String? ?: System.getenv("OSSRH_USERNAME")
+                password = project.findProperty("ossrhPassword") as String? ?: System.getenv("OSSRH_PASSWORD")
+            }
+        }
+    }
+}
+
+signing {
+    useInMemoryPgpKeys(
+        project.findProperty("signingKey") as String? ?: System.getenv("GPG_PRIVATE_KEY"),
+        project.findProperty("signingPassword") as String? ?: System.getenv("GPG_PASSWORD")
+    )
+    sign(publishing.publications["mavenJava"])
 }

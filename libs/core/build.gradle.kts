969b421bcdd2715820857bf19631e5fe62c15010
Arkanium77 <arkanium77@gmail.com>
2025-06-07 16:27:53 +0300

    dependencies updated

--- libs/core/build.gradle.kts
+++ libs/core/build.gradle.kts
@@ -5,15 +5,21 @@ plugins {
 group = "team.isaz.existence"
 version = "0.1.0-SNAPSHOT"
 
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation(platform("org.junit:junit-bom:5.12.2"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-    testImplementation("org.assertj:assertj-core:3.24.2")
-    testImplementation("org.mockito:mockito-core:4.11.0")
+    testImplementation("org.assertj:assertj-core:3.27.3")
+    testImplementation("org.mockito:mockito-core:5.18.0")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.test {
